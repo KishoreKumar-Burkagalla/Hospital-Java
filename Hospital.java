a9791aa612ee0dcca381cb793b28cdd9bb8fe39a
Kishore Kumar Burkagalla <kishore.b@trimindtech.com>
2022-09-19 23:19:21 +0530

    second commit

--- Hospital.java
+++ Hospital.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import java.util.*;
 
-public class Hosp {
+public class Hospital {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
